dac8fb7086645a7d115c0ad78a1db45c3ebda9ea
ANISDHIYAEDDINE_BOUDIAF <dhiaeboudiaf@gmail.com>
2021-06-03 15:05:51 +0200

    that's it

--- api/test/hello.test.js
+++ api/test/hello.test.js
@@ -2,6 +2,7 @@ const axios = require('axios')
 
 test('test', async () => {
    try {
+       if (process.env.Hello == "bae") console.log("Got you bitch")
        let response = await axios.get(process.env.SERVER_1);
        expect(response.data).toBe("server-1")
        response = await axios.get(process.env.SERVER_2);
