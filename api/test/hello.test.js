0e7cc05dd5d66a5a6a00f3ac2bd6740a8706797c
ANISDHIYAEDDINE_BOUDIAF <dhiaeboudiaf@gmail.com>
2021-06-03 15:22:59 +0200

    hello

--- api/test/hello.test.js
+++ api/test/hello.test.js
@@ -1,10 +1,5 @@
 const axios = require('axios')
 
 test('test', async () => {
-   try {
-       expect(process.env.Hello).toBe("bae");
-      
-   } catch (error) {
-       console.error(error);
-   }
+       expect(process.env.Hello).toBe("bae");  
 })
