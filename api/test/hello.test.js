9f57e3ef30c658339eb966b292fdd25a7f4fecc8
ANISDHIYAEDDINE_BOUDIAF <dhiaeboudiaf@gmail.com>
2021-06-03 11:32:08 +0200

    quick test

--- api/test/hello.test.js
+++ api/test/hello.test.js
@@ -2,7 +2,8 @@ const axios = require('axios')
 
 test('hello hello', async () => {
    try {
-       console.log(process.env);
+       console.log(process.env );
+       console.log('-------')
        let response = await axios.get(process.env.SERVER_1);
        expect(response.data).toBe("server-1")
        response = await axios.get(process.env.SERVER_2);
